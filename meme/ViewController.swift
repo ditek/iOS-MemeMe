7cf7e6109b674f2e0fef5077325cea29bfee8998
Diaa <ditek@users.noreply.github.com>
2020-12-19 22:25:28 +0100

    Add main UI components

--- meme/ViewController.swift
+++ meme/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
+    
+    // MARK: Lifecycle
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    
+    // MARK: Notofication handling
+    
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
 
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        print("keyboard show")
+        
+        // Sometimes the show event is triggered twice.
+        // We check the animation duration and if it is 0 then it's a fake one
+        if let duration = notification.userInfo?["UIKeyboardAnimationDurationUserInfoKey"] {
+            if let duration = duration as? Float {
+                if duration > 0 {
+                    view.frame.origin.y -= getKeyboardHeight(notification)
+                }
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        print("keyboard hide")
+        view.frame.origin.y += getKeyboardHeight(notification)
+    }
+
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    
+    // MARK: TextField callbacks
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
 }
 
