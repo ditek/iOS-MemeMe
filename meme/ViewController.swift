3a1b7ed47eaf571747b4b1339439665eb7a345d6
Diaa <ditek@users.noreply.github.com>
2020-12-20 13:02:58 +0100

    Add image picker and share functionality

--- meme/ViewController.swift
+++ meme/ViewController.swift
@@ -7,17 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
-    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var topBar: UINavigationBar!
+    @IBOutlet weak var bottomBar: UIToolbar!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     // MARK: Lifecycle
 
     
     override func viewWillAppear(_ animated: Bool) {
         subscribeToKeyboardNotifications()
+        shareButton.isEnabled = false
+        if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            cameraButton.isEnabled = false
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -60,14 +68,76 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     
-    // MARK: TextField callbacks
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-//        textField.text = ""
-    }
+    // MARK: Callbacks
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imageView.image = image
+            picker.dismiss(animated: true, completion: nil)
+            shareButton.isEnabled = true
+        } else {
+            print("Error while displaying image")
+        }
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func selectImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func selectFromCamera(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func share() {
+        topBar.isHidden = true
+        bottomBar.isHidden = true
+        let memeImage = generateMemedImage()
+        let activity = UIActivityViewController(activityItems: [memeImage], applicationActivities: nil)
+        activity.completionWithItemsHandler = { (type, completed, items, error) in
+            if completed {
+                let meme = Meme(
+                    topText: self.topText.text!,
+                    bottomText: self.bottomText.text!,
+                    originalImage: self.imageView.image!,
+                    memedImage: memeImage
+                )
+                print(meme)
+            }
+        }
+        present(activity, animated: true, completion: nil)
+        topBar.isHidden = false
+        bottomBar.isHidden = false
+    }
+    
+    @IBAction func resetUI(_ sender: Any) {
+        topText.text = ""
+        bottomText.text = ""
+        imageView.image = nil
+        shareButton.isEnabled = false
+    }
+    
+    
+    // MARK: Other
+    
+    // Render view to an image
+    func generateMemedImage() -> UIImage {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        return memedImage
     }
 }
 
---
+++ meme/meme.swift
@@ -0,0 +1,23 @@
+//
+//  meme.swift
+//  meme
+//
+//  Created by Diaa on 20/12/2020.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String?
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+    
+//    init(topText: String, bottomText: String, image: UIImage) {
+//        self.topText = topText
+//        self.bottomText = bottomText
+//        self.originalImage = image
+//    }
+    
+}
